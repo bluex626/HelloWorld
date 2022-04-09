4d42d77fea8fc374516bfec00873e165885b4021
temp <temp@MRxBLUEX626>
2022-04-08 19:46:23 -0700

    Hello

---
+++ HelloWorld/src/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		System.out.println("This is a test");
+		
+		
+	}
+
+}
